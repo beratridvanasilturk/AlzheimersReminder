194368422e1b5ed56da7325b16232ab2fe20322a
beratridvanasilturk <br.asltrk@gmail.com>
2023-08-25 18:19:29 +0300

    Core Data Data Saving | Images "loading to memory" with Core Data
    Core Data Data Saving

--- AlzheimersReminder/AlzheimersReminder/DetailsVC.swift
+++ AlzheimersReminder/AlzheimersReminder/DetailsVC.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var numberTextField: UITextField!
     @IBOutlet weak var name2TextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
@@ -16,6 +17,9 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         
         
         // Recognizer
@@ -29,19 +33,19 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
         imageView.addGestureRecognizer(imageTapRecognizer)
         
-        
     }
     
     // Ui'da bosluga tiklayinca klavyeyi kapatma func
     @objc func hideKeyboard() {
         view.endEditing(true)
     }
+    
     // Kullaniciyi galeriden image sectirme func
     @objc func selectImage() {
         
         let picker = UIImagePickerController()
         picker.delegate = self
-            //  UIImagePickerControllerDelegate, UINavigationControllerDelegate classimiza eklendi. UINavContDelegate galeriden image sectikten sonra ekranin kapanmasinda ve tekrar uygulamaya donmekte kullanilacak.
+        //  UIImagePickerControllerDelegate, UINavigationControllerDelegate classimiza eklendi. UINavContDelegate galeriden image sectikten sonra ekranin kapanmasinda ve tekrar uygulamaya donmekte kullanilacak.
         // Galeriye gidip image secme islemini yapar
         picker.sourceType = .photoLibrary
         // Kullanici sectigi image'i editleyebilmesini saglar
@@ -50,6 +54,7 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         present(picker, animated: true)
         
     }
+    
     // Gorsel secildikten sonra image view'a gorselin aktarilmasini saglar
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         // .editedImage yerine originalImage'i de kullanabilirdik. Genellikle ikisi kullanilir ama bu projede editi actigimiz icin editingImage daha saglikli olacaktir
@@ -57,8 +62,34 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         self.dismiss(animated: true)
     }
     
-    
+    // Core Data ile secilen image'i hafizaya atar
     @IBAction func saveButtonTapped(_ sender: Any) {
+        
+        // Bizim context'e ulasabilmemiz icin appdelegate'i bir degisken olarak atamamiz gerekir. Context: AppDelegate -> func saveContext
+        // AppDelegate'i degisken olarak tanimladik
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        // persistentContainer.viewContext ile AppDelegate'deki supporting context'leri kullanabilir hale geliriz
+        let context = appDelegate.persistentContainer.viewContext
+        // Entity name'i AlzheimersReminder.xcdatamodeld dosyasindaki basliktan aldik, veriyi nereye kaydececegimizi belirlemek icin kullandik NSEntityDescription.insertObj'yi
+        let newImage = NSEntityDescription.insertNewObject(forEntityName: "Images", into: context)
+        
+        // Attributes
+        // Attributeleri AlzheimersReminder.xcdatamodeld dosyasinda olusturdugumuz projemizde olmasini istedigimiz ozelliklerden aldik ki ona gore kaydetmek istedigimiz tum her seyi unique sekilde save edebilelim.
+        newImage.setValue(nameTextField.text, forKey: "name")
+        newImage.setValue(name2TextField.text, forKey: "name2")
+        newImage.setValue(UUID(), forKey: "id")
+        if let num = Int(numberTextField.text!) {
+            newImage.setValue(num, forKey: "number")
+        }
+        // compressionQuality image'in kaydetme kalitesini duzenler, sıkıstırma islemidir.
+        let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        newImage.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("Sucseed Image Save")
+        } catch {
+            print("Error: Image Save")
+        }
     }
-    
 }
