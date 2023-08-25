03c35d0b8d15cbaee07e5a791efe5e418049f744
beratridvanasilturk <br.asltrk@gmail.com>
2023-08-25 12:27:08 +0300

    Added 2 ViewController and Add Buttton Segue

---
+++ AlzheimersReminder/AlzheimersReminder/DetailsVC.swift
@@ -0,0 +1,25 @@
+//
+//  DetailsVC.swift
+//  AlzheimersReminder
+//
+//  Created by Berat Rıdvan Asiltürk on 25.08.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var name2TextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+}
---
+++ AlzheimersReminder/AlzheimersReminder/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AlzheimersReminder
+//
+//  Created by Berat Rıdvan Asiltürk on 25.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonTapped))
+    }
+    
+    @objc func addButtonTapped() {
+        performSegue(withIdentifier: "toDetailVC", sender: nil)
+    }
+}
+
