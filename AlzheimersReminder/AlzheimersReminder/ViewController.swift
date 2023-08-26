5af23b751fb71ae94bb8c5b0367e608dd2116f28
beratridvanasilturk <br.asltrk@gmail.com>
2023-08-26 11:39:01 +0300

    Swipe to Delete |Commit (EditingStyle) Used for Delete Item in Core Data

--- AlzheimersReminder/AlzheimersReminder/ViewController.swift
+++ AlzheimersReminder/AlzheimersReminder/ViewController.swift
@@ -122,5 +122,60 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            // Core Data'dan  verileri bulup silmemiz gerekir
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            // Fetch request'in olusturulma amaci ilgili veriyi cekip silmek icindir
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Images")
+            // Predicate kullanim amaci sadece 1 veriyi silmek isteyisimiz, o veriyi bulup cekip silecegiz.
+            // Nereye tiklandiysa onun id'sini bulmamizi saglar
+            let idString = idArray[indexPath.row].uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            // Cachelarle ilgili uygulamaya hiz ve verim saglar
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+            let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                              
+                    for result in results as! [NSManagedObject] {
+                                  
+                        if let id = result.value(forKey: "id") as? UUID {
+                            // Iki id'nin de birbirine esit olmasini saglama aliyoruz
+                            if id == idArray[indexPath.row] {
+                            // Core Data'dan result'u siler
+                            context.delete(result)
+                            nameArray.remove(at: indexPath.row)
+                            idArray.remove(at: indexPath.row)
+                            // TableView'i guncelleriz
+                            self.tableView.reloadData()
+                            
+                            // Cora Data'da yapilan degisiklikleri kaydeder
+                            do {
+                                try context.save()
+                                              
+                            } catch {
+                            print("error")
+                                          }
+                            // Eger aradigim seyi bulup sildiysem for loop'un devam etmesini kirmak icin kullandik
+                            break
+                            
+                            }
+                                      
+                        }
+                                  
+                                  
+                    }
+                              
+                              
+                }
+            } catch {
+                print("error")
+            }
+        }
+    }
 }
-
