2fb32b870cb5cbca8c3f4a9b511dab90f33bd1ff
beratridvanasilturk <br.asltrk@gmail.com>
2023-08-27 03:51:09 +0300

    Added Location Settings & Permission

--- AlzheimersReminder/AlzheimersReminder/LocationViewController.swift
+++ AlzheimersReminder/AlzheimersReminder/LocationViewController.swift
@@ -7,15 +7,30 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class LocationViewController: UIViewController,MKMapViewDelegate {
+class LocationViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDelegate {
     //MARK: -Outlets
     @IBOutlet weak var mapView: MKMapView!
+    // "CoreLocation Lokasyon Manageri" eklendi
+    var locationManager = CLLocationManager()
     
     //MARK: -Funcs
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         mapView.delegate = self
+        locationManager.delegate = self
+        // Kullanicinin lokasyonunun keskinligi (metre cinsi) belirlenir
+        // Uygulamalarin amaclarina gore buradaki lokasyon sapmasi degisebilir ancak bizim projemizde en detayli konum bulmak hayati oneme sahiptir.
+        // En detayli konum icin "kCLLocationAccuracyBest" kullanilir
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // Kullanicidan izin istenir
+        // Yine uygulamalarin amaclarina gore izin isteme sıklıgı degisebilir. Biz esnek davranip uygulama indirildiginde baslangicta bir kere sormayi tercih edecegiz.
+        locationManager.requestAlwaysAuthorization()
+        // Kullanicinin lokasyonu alinmaya baslanir.
+        locationManager.startUpdatingLocation()
+        
         
     }
 }
