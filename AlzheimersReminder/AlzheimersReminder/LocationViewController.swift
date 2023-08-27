e683de44ef41741d43b427582653bbe8c6d3ae83
beratridvanasilturk <br.asltrk@gmail.com>
2023-08-27 04:40:35 +0300

    Updated Privacy and Added .plist Message For Location Access

--- AlzheimersReminder/AlzheimersReminder/LocationViewController.swift
+++ AlzheimersReminder/AlzheimersReminder/LocationViewController.swift
@@ -26,11 +26,25 @@ class LocationViewController: UIViewController,MKMapViewDelegate, CLLocationMana
         // En detayli konum icin "kCLLocationAccuracyBest" kullanilir
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         // Kullanicidan izin istenir
-        // Yine uygulamalarin amaclarina gore izin isteme sıklıgı degisebilir. Biz esnek davranip uygulama indirildiginde baslangicta bir kere sormayi tercih edecegiz.
-        locationManager.requestAlwaysAuthorization()
+        // Yine uygulamalarin amaclarina gore izin isteme sıklıgı degisebilir. Bu projede kullanicinin guvenligi icin "requestWhenInUseAuthorization" kullanacagiz ancak .plist'e bir uyari mesaji gondererek uygulama kullanilirken konuma surekli izin vermesi gerektigini hatirlatacagiz.
+        locationManager.requestWhenInUseAuthorization()
         // Kullanicinin lokasyonu alinmaya baslanir.
         locationManager.startUpdatingLocation()
+    }
+    // DidUptadeLocations: Guncellenen lokasyonlari array icerisinde verern hazir functur
+    // CLLocation enlem ve boylam icerir
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
+        // Kullanicinin anlik lokasyonunu aliriz
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        // Lokasyonu gosterebilmek icin zoom seviyesi olusturmaliyiz
+        // Span: yukseklik ve genislik yani zoom anlamina gelir
+        // 0.2 degerleri ne kadar kuculurse o kadar yakinlastirilir
+        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        //  Region: Bolge anlamindadir, Bolge merkezini olusturdumuz lokasyona zoomunu da olusturdugumuz zoom'a atadik
+        let region = MKCoordinateRegion(center: location, span: span)
+        // Belirtilen enlem ve boylama yani bolgeye zoomlama islemini tamamlar
+        mapView.setRegion(region, animated: true)
         
     }
 }
