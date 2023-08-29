c3ec4a7c74ae94020a8b0c8ca5ef7fd655f29413
beratridvanasilturk <br.asltrk@gmail.com>
2023-08-29 22:54:32 +0300

    Fixed Image Entity Issue | Because of xcdatamodeld Name Didn't Macthed
    xcdatamodeld : Data Model File | Core Data

--- AlzheimersReminder/AlzheimersReminder/LocationViewController.swift
+++ AlzheimersReminder/AlzheimersReminder/LocationViewController.swift
@@ -1,136 +1,136 @@
+////
+////  LocationViewController.swift
+////  AlzheimersReminder
+////
+////  Created by Berat Rıdvan Asiltürk on 27.08.2023.
+////
 //
-//  LocationViewController.swift
-//  AlzheimersReminder
+//import UIKit
+//import MapKit
+//import CoreLocation
+//import CoreData
 //
-//  Created by Berat Rıdvan Asiltürk on 27.08.2023.
+//class LocationViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDelegate {
 //
-
-import UIKit
-import MapKit
-import CoreLocation
-import CoreData
-
-class LocationViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDelegate {
-
-    var chosenLocationTitle = ""
-    var chosenLongitutePoint = Double()
-    var chosenLatitudePoint = Double()
-    
-    var chosenLocationId : UUID?
-    var chosenLocation = ""
-    
-    @IBOutlet weak var locationTitleTextField: UITextField!
-    
-    @IBOutlet weak var locationSubtitleTextField: UITextField!
-    //MARK: -Outlets
-    @IBOutlet weak var mapView: MKMapView!
-    // "CoreLocation Lokasyon Manageri" eklendi
-    var locationManager = CLLocationManager()
-    
-    //MARK: -Funcs
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        mapView.delegate = self
-        locationManager.delegate = self
-        // Kullanicinin lokasyonunun keskinligi (metre cinsi) belirlenir
-        // Uygulamalarin amaclarina gore buradaki lokasyon sapmasi degisebilir ancak bizim projemizde en detayli konum bulmak hayati oneme sahiptir.
-        // En detayli konum icin "kCLLocationAccuracyBest" kullanilir
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        // Kullanicidan izin istenir
-        // Yine uygulamalarin amaclarina gore izin isteme sıklıgı degisebilir. Bu projede kullanicinin guvenligi icin "requestWhenInUseAuthorization" kullanacagiz ancak .plist'e bir uyari mesaji gondererek uygulama kullanilirken konuma surekli izin vermesi gerektigini hatirlatacagiz.
-        locationManager.requestWhenInUseAuthorization()
-        // Kullanicinin lokasyonu alinmaya baslanir.
-        locationManager.startUpdatingLocation()
-        
-        // Gesture Recognizer'i burada haritaya pin eklemek icin kullanacagiz
-        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(choseLocation(gestureRecognizer:)))
-        
-        // Kac saniye basildiktan sonra pin atacagimizi ayarlariz
-        gestureRecognizer.minimumPressDuration = 1
-        
-        mapView.addGestureRecognizer(gestureRecognizer)
-        
-            
-
-    }
-    
-
-        // MARK: TRUE
-        // DidUptadeLocations: Guncellenen lokasyonlari array icerisinde verern hazir functur
-        // CLLocation enlem ve boylam icerir
-        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            
-            // Kullanicinin anlik lokasyonunu aliriz
-            let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
-            // Lokasyonu gosterebilmek icin zoom seviyesi olusturmaliyiz
-            // Span: yukseklik ve genislik yani zoom anlamina gelir
-            // 0.02 degerleri ne kadar kuculurse o kadar yakinlastirilir
-            let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
-            //  Region: Bolge anlamindadir, Bolge merkezini olusturdumuz lokasyona zoomunu da olusturdugumuz zoom'a atadik
-            let region = MKCoordinateRegion(center: location, span: span)
-            // Belirtilen enlem ve boylama yani bolgeye zoomlama islemini tamamlar
-            mapView.setRegion(region, animated: true)
-            
-        }
-
-// choseLocation'a input olarak UILongPressGestureRecognizer'i vermemizin amaci o fonksiyon icerisinde UILongPressGestureRecognizer'in ozelliklerine "." koyduktan sonra direkt kendi methodlarina kendi attributelerine ulasabilmek icindir.
-// MARK: TRUE
-    @objc func choseLocation(gestureRecognizer: UILongPressGestureRecognizer) {
-        // Gesture Recognizer baslama durumunu kontrol eder
-        if gestureRecognizer.state == .began {
-            // Toucked point kullanicinin dokunmus oldugu noktanin lokasyonunu almak icin kullanilir
-            let touchedPoint = gestureRecognizer.location(in: self.mapView)
-            // Dokunulan point'i coordinate'a cevirir
-            let touchedCoordinate = self.mapView.convert(touchedPoint, toCoordinateFrom: self.mapView)
-            
-            // CORE DATA'YA ENLEM BOYLAM ATAMAK ICIN DATAYI BURDAN CEKTIK
-            // Core Dataya long ve latitude'u kaydetmek icin clasimizda birer degisken olusturup burdan aldigimiz long ve lat'leri o degiskene atar ve daha sonrasinda SaveButton'dan Core Data'ya aktaririz
-            // Save Button Tapped ' da Attribute'lerde Core Data'ya Latitude ve Longitute Eklemek Ici Kullandik
-            chosenLatitudePoint = touchedCoordinate.latitude
-            chosenLongitutePoint = touchedCoordinate.longitude
-            
-            // Olusturdugumuz pini nereye atamamiz gerektigini ayarliyoruz
-            // Annotation: Pin
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = touchedCoordinate
-            annotation.title = locationTitleTextField.text
-            annotation.subtitle = locationSubtitleTextField.text
-            
-            self.mapView.addAnnotation(annotation)
-            
-        }
-    }
-    
-    
-    
-    @IBAction func saveButtonTapped(_ sender: Any) {
-    
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
-        // Seperated Entity Names with Image View Cont (DetailVC)
-        let newLocation = NSEntityDescription.insertNewObject(forEntityName: "MyEntity", into: context)
-        
-        
-        // Attributes
-        
-        newLocation.setValue(locationTitleTextField.text, forKey: "title")
-        newLocation.setValue(locationSubtitleTextField.text, forKey: "subtitle")
-        
-        // Enlem ve boylami chose location func'tan aliriz direkt
-        newLocation.setValue(chosenLatitudePoint, forKey: "latitude")
-        newLocation.setValue(chosenLongitutePoint, forKey: "longitute")
-        
-        newLocation.setValue(UUID(), forKey: "id2")
-      
-        
-        do {
-            try context.save()
-            print("Core Data Saving Success")
-        } catch{
-            print("Core Data Saving Error")
-        }
-        
-    }
-    
-}
+//    var chosenLocationTitle = ""
+//    var chosenLongitutePoint = Double()
+//    var chosenLatitudePoint = Double()
+//    
+//    var chosenLocationId : UUID?
+//    var chosenLocation = ""
+//    
+//    @IBOutlet weak var locationTitleTextField: UITextField!
+//    
+//    @IBOutlet weak var locationSubtitleTextField: UITextField!
+//    //MARK: -Outlets
+//    @IBOutlet weak var mapView: MKMapView!
+//    // "CoreLocation Lokasyon Manageri" eklendi
+//    var locationManager = CLLocationManager()
+//    
+//    //MARK: -Funcs
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        mapView.delegate = self
+//        locationManager.delegate = self
+//        // Kullanicinin lokasyonunun keskinligi (metre cinsi) belirlenir
+//        // Uygulamalarin amaclarina gore buradaki lokasyon sapmasi degisebilir ancak bizim projemizde en detayli konum bulmak hayati oneme sahiptir.
+//        // En detayli konum icin "kCLLocationAccuracyBest" kullanilir
+//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        // Kullanicidan izin istenir
+//        // Yine uygulamalarin amaclarina gore izin isteme sıklıgı degisebilir. Bu projede kullanicinin guvenligi icin "requestWhenInUseAuthorization" kullanacagiz ancak .plist'e bir uyari mesaji gondererek uygulama kullanilirken konuma surekli izin vermesi gerektigini hatirlatacagiz.
+//        locationManager.requestWhenInUseAuthorization()
+//        // Kullanicinin lokasyonu alinmaya baslanir.
+//        locationManager.startUpdatingLocation()
+//        
+//        // Gesture Recognizer'i burada haritaya pin eklemek icin kullanacagiz
+//        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(choseLocation(gestureRecognizer:)))
+//        
+//        // Kac saniye basildiktan sonra pin atacagimizi ayarlariz
+//        gestureRecognizer.minimumPressDuration = 1
+//        
+//        mapView.addGestureRecognizer(gestureRecognizer)
+//        
+//            
+//
+//    }
+//    
+//
+//        // MARK: TRUE
+//        // DidUptadeLocations: Guncellenen lokasyonlari array icerisinde verern hazir functur
+//        // CLLocation enlem ve boylam icerir
+//        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//            
+//            // Kullanicinin anlik lokasyonunu aliriz
+//            let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+//            // Lokasyonu gosterebilmek icin zoom seviyesi olusturmaliyiz
+//            // Span: yukseklik ve genislik yani zoom anlamina gelir
+//            // 0.02 degerleri ne kadar kuculurse o kadar yakinlastirilir
+//            let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+//            //  Region: Bolge anlamindadir, Bolge merkezini olusturdumuz lokasyona zoomunu da olusturdugumuz zoom'a atadik
+//            let region = MKCoordinateRegion(center: location, span: span)
+//            // Belirtilen enlem ve boylama yani bolgeye zoomlama islemini tamamlar
+//            mapView.setRegion(region, animated: true)
+//            
+//        }
+//
+//// choseLocation'a input olarak UILongPressGestureRecognizer'i vermemizin amaci o fonksiyon icerisinde UILongPressGestureRecognizer'in ozelliklerine "." koyduktan sonra direkt kendi methodlarina kendi attributelerine ulasabilmek icindir.
+//// MARK: TRUE
+//    @objc func choseLocation(gestureRecognizer: UILongPressGestureRecognizer) {
+//        // Gesture Recognizer baslama durumunu kontrol eder
+//        if gestureRecognizer.state == .began {
+//            // Toucked point kullanicinin dokunmus oldugu noktanin lokasyonunu almak icin kullanilir
+//            let touchedPoint = gestureRecognizer.location(in: self.mapView)
+//            // Dokunulan point'i coordinate'a cevirir
+//            let touchedCoordinate = self.mapView.convert(touchedPoint, toCoordinateFrom: self.mapView)
+//            
+//            // CORE DATA'YA ENLEM BOYLAM ATAMAK ICIN DATAYI BURDAN CEKTIK
+//            // Core Dataya long ve latitude'u kaydetmek icin clasimizda birer degisken olusturup burdan aldigimiz long ve lat'leri o degiskene atar ve daha sonrasinda SaveButton'dan Core Data'ya aktaririz
+//            // Save Button Tapped ' da Attribute'lerde Core Data'ya Latitude ve Longitute Eklemek Ici Kullandik
+//            chosenLatitudePoint = touchedCoordinate.latitude
+//            chosenLongitutePoint = touchedCoordinate.longitude
+//            
+//            // Olusturdugumuz pini nereye atamamiz gerektigini ayarliyoruz
+//            // Annotation: Pin
+//            let annotation = MKPointAnnotation()
+//            annotation.coordinate = touchedCoordinate
+//            annotation.title = locationTitleTextField.text
+//            annotation.subtitle = locationSubtitleTextField.text
+//            
+//            self.mapView.addAnnotation(annotation)
+//            
+//        }
+//    }
+//    
+//    
+//    
+//    @IBAction func saveButtonTapped(_ sender: Any) {
+//    
+//        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//        let context = appDelegate.persistentContainer.viewContext
+//        // Seperated Entity Names with Image View Cont (DetailVC)
+//        let newLocation = NSEntityDescription.insertNewObject(forEntityName: "MyEntity", into: context)
+//        
+//        
+//        // Attributes
+//        
+//        newLocation.setValue(locationTitleTextField.text, forKey: "title")
+//        newLocation.setValue(locationSubtitleTextField.text, forKey: "subtitle")
+//        
+//        // Enlem ve boylami chose location func'tan aliriz direkt
+//        newLocation.setValue(chosenLatitudePoint, forKey: "latitude")
+//        newLocation.setValue(chosenLongitutePoint, forKey: "longitute")
+//        
+//        newLocation.setValue(UUID(), forKey: "id2")
+//      
+//        
+//        do {
+//            try context.save()
+//            print("Core Data Saving Success")
+//        } catch{
+//            print("Core Data Saving Error")
+//        }
+//        
+//    }
+//    
+//}
